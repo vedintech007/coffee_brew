1f73c91282855d9a81fb048093420d749c9d4d57
Eklu Victor Dziedzorm <55409753+Victorspy-web@users.noreply.github.com>
2023-02-09 09:16:35 +0000

    added provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ninja_brew_crew/firebase_options.dart';
+import 'package:ninja_brew_crew/models/user.dart';
 import 'package:ninja_brew_crew/screens/wrapper.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:ninja_brew_crew/services/auth.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,14 +20,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          elevation: 0.0,
+    return StreamProvider.value(
+      value: AuthService().user,
+      initialData: null,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            elevation: 0.0,
+            backgroundColor: Colors.amber[900],
+          ),
         ),
+        home: const Wrapper(),
       ),
-      home: const Wrapper(),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:ninja_brew_crew/models/user.dart';
 import 'package:ninja_brew_crew/screens/authenticate/authenticate.dart';
 import 'package:ninja_brew_crew/screens/home/home.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserModel?>(context);
+
+    print(user);
     // return either home or authentice widget
-    return StreamBuilder<Object>(
-        stream: null,
-        builder: (context, snapshot) {
-          return Authenticate();
-        });
+    return Authenticate();
   }
 }
